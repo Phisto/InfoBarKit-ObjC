c1548f9265b926231b31277f0fa3a9568222bd6a
Simon Gaus <phisto05@gmail.com>
2019-06-11 01:28:09 +0200

    Add public header to framework header

--- InfoBarKit/InfoBarKit.h
+++ InfoBarKit/InfoBarKit.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char InfoBarKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <InfoBarKit/PublicHeader.h>
 
-
+#import <InfoBarKit/SGInfoBar.h>
+#import <InfoBarKit/SGInfoBarTask.h>
