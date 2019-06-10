33a2424c7451a5aac6f6f175a5c9ff2c15d9ed7c
Simon Gaus <phisto05@gmail.com>
2019-06-11 00:13:11 +0200

    Add missing files

---
+++ InfoBarKit/InfoBarKit.h
@@ -0,0 +1,19 @@
+//
+//  InfoBarKit.h
+//  InfoBarKit
+//
+//  Created by Simon Gaus on 30.03.19.
+//  Copyright © 2019 Simon Gaus. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for InfoBarKit.
+FOUNDATION_EXPORT double InfoBarKitVersionNumber;
+
+//! Project version string for InfoBarKit.
+FOUNDATION_EXPORT const unsigned char InfoBarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfoBarKit/PublicHeader.h>
+
+
